d91e331ddd96e92424daf0497a42caa5d8171500
Liam Greene <wgreene@guilford.edu>
2023-05-03 13:39:12 -0400

    changed the App.java name

--- src/main/java/greene/ctis310/FlashcardMain.java
+++ src/main/java/greene/ctis310/FlashcardMain.java
@@ -11,7 +11,7 @@ import java.io.IOException;
 /**
  * JavaFX App
  */
-public class App extends Application {
+public class FlashcardMain extends Application {
 
     private static Scene scene;
 
@@ -27,7 +27,7 @@ public class App extends Application {
     }
 
     private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(FlashcardMain.class.getResource(fxml + ".fxml"));
         return fxmlLoader.load();
     }
 
