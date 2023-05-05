d1f732316f0491dedf87fb1b5875ade8ff0cfc2a
Liam Greene <wgreene@guilford.edu>
2023-05-05 10:33:20 -0400

    Changes to GUI

--- src/main/java/greene/ctis310/PrimaryController.java
+++ src/main/java/greene/ctis310/PrimaryController.java
@@ -2,11 +2,18 @@ package greene.ctis310;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 public class PrimaryController {
 
     @FXML
-    private void switchToSecondary() throws IOException {
-        FlashcardMain.setRoot("secondary");
+    private void createCardSet() throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(FlashcardMain.class.getResource("secondary.fxml"));
+        Stage stage = new Stage();
+        stage.setScene(new Scene(fxmlLoader.load()));
+        stage.setTitle("New Card Set");
+        stage.show();
     }
 }
