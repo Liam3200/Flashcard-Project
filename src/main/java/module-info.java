25e1195024fa211bd1572c122dc267dfc1bf609a
Liam Greene <wgreene@guilford.edu>
2023-05-10 13:13:45 -0400

    Fixed warning with Stage object

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module greene.ctis310 {
     requires javafx.controls;
     requires javafx.fxml;
     requires org.apache.commons.io;
+    requires transitive javafx.graphics;
 
     opens greene.ctis310 to javafx.fxml;
     exports greene.ctis310;
